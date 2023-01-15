9dd1d121028ea34101b10c4a843c5b9b00377724
BABASTAS728 <Aberezkin954@gmail.com>
2023-01-15 18:26:06 +0300

    homework films

--- app/src/main/java/com/example/listoffilms/MainActivity.kt
+++ app/src/main/java/com/example/listoffilms/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.listoffilms
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val films = Server.getFilms()
+        val recycler = findViewById<RecyclerView>(R.id.recycler)
+        val adapter = MyAdapter(films)
+        recycler.adapter = adapter
+        recycler.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
     }
 }
---
+++ app/src/main/java/com/example/listoffilms/MyAdapter.kt
@@ -0,0 +1,19 @@
+package com.example.listoffilms
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MyAdapter(val films: List<Film>): RecyclerView.Adapter<MyViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+      holder.bind(films[position])
+    }
+
+    override fun getItemCount(): Int = films.size
+}
